22c851b39cef3feb1af292fe804c0757b8cdc689
nmhtet <36500446+nmhtet@users.noreply.github.com>
2025-11-23 19:41:23 +0800

    Refactor agent initialization and load environment variables
    Refactor interior design agent initialization to use dotenv and a dedicated function for agent creation.

--- src/app/agents/interiorDesignAgent_initializer.py
+++ src/app/agents/interiorDesignAgent_initializer.py
@@ -1,14 +1,15 @@
 import os
 import sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 from azure.ai.projects import AIProjectClient
 from azure.identity import DefaultAzureCredential
-from azure.ai.agents.models import FunctionTool, ToolSet
-from typing import Callable, Set, Any
-from tools.imageCreationTool import create_image
+from azure.ai.agents.models import ToolSet
+from dotenv import load_dotenv
+from agent_processor import create_function_tool_for_agent
+from agent_initializer import initialize_agent
 
-# Load the prompt instructions for the interior design agent from a file
-# path = r'prompts\InteriorDesignAgentPrompt.txt'
+load_dotenv()
+
 ID_PROMPT_TARGET = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'prompts', 'InteriorDesignAgentPrompt.txt')
 with open(ID_PROMPT_TARGET, 'r', encoding='utf-8') as file:
     ID_PROMPT = file.read()
@@ -20,43 +21,17 @@ project_client = AIProjectClient(
     credential=DefaultAzureCredential(),
 )
 
-# Define the set of user-defined callable functions to use as tools
-user_functions: Set[Callable[..., Any]] = {
-    create_image
-}
-
-# Initialize toolset and enable auto function calling with the tools
-functions = FunctionTool(user_functions)
+# Define the set of user-defined callable functions to use as tools (from MCP client)
+functions = create_function_tool_for_agent("interior_designer")
 toolset = ToolSet()
 toolset.add(functions)
 project_client.agents.enable_auto_function_calls(tools=functions)
 
-# Create the agent using a specific deployment, name, instructions, and toolset
-with project_client:
-
-    agent_exists = False
-    if agent_id:
-        # Check if agent exists.
-        agent = project_client.agents.get_agent(agent_id)
-        print(f"Retrieved existing agent, ID: {agent.id}")
-        agent_exists = True
-        
-    if agent_exists:
-        agent = project_client.agents.update_agent(
-            agent_id=agent.id,
-            model=model,
-            name=name,
-            instructions=instructions,
-            toolset=toolset
-        )
-        print(f"Updated {env_var_name} agent, ID: {agent.id}")
-
-    else:
-
-        agent = project_client.agents.create_agent(
-        model=os.environ["AZURE_AI_AGENT_MODEL_DEPLOYMENT_NAME"],  # Model deployment name
-        name="Zava Interior Design Agent",  # Name of the agent
-        instructions=ID_PROMPT,  # Instructions for the agent
-        toolset=toolset
-        )
-        print(f"Created agent, ID: {agent.id}")
+initialize_agent(
+    project_client=project_client,
+    model=os.environ["AZURE_AI_AGENT_MODEL_DEPLOYMENT_NAME"],
+    env_var_name="interior_designer",
+    name="Zava Interior Design Agent",
+    instructions=ID_PROMPT,
+    toolset=toolset
+)
