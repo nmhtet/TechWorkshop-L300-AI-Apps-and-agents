1c55a86623c42283ac5a8bdca904f89389e1fcaf
nmhtet <36500446+nmhtet@users.noreply.github.com>
2025-11-23 17:15:40 +0800

    Refactor agent initialization logic for clarity

--- src/app/agents/customerLoyaltyAgent_initializer.py
+++ src/app/agents/customerLoyaltyAgent_initializer.py
@@ -34,33 +34,31 @@ toolset = ToolSet()
 toolset.add(functions)
 project_client.agents.enable_auto_function_calls(tools=functions)
 
-with project_client:
 
-    agent_exists = False
-    if agent_id:
-        # Check if agent exists.
-        agent = project_client.agents.get_agent(agent_id)
-        print(f"Retrieved existing agent, ID: {agent.id}")
-        agent_exists = True
+with project_client:
+        agent_exists = False
+        if agent_id:
+            # Check if agent exists.
+            agent = project_client.agents.get_agent(agent_id)
+            print(f"Retrieved existing agent, ID: {agent.id}")
+            agent_exists = True
         
-    if agent_exists:
-        agent = project_client.agents.update_agent(
-            agent_id=agent.id,
+        if agent_exists:
+            agent = project_client.agents.update_agent(
+                agent_id=agent.id,
+                model=model,
+                name=name,
+                instructions=instructions,
+                toolset=toolset
+            )
+            print(f"Updated {env_var_name} agent, ID: {agent.id}")
+        else:
+            agent = project_client.agents.create_agent(
             model=model,
             name=name,
             instructions=instructions,
             toolset=toolset
-        )
-        print(f"Updated {env_var_name} agent, ID: {agent.id}")
-    
-    else:
-
-        agent = project_client.agents.create_agent(
-        model=os.getenv("AZURE_AI_AGENT_MODEL_DEPLOYMENT_NAME"),  # Model deployment name
-        name="Zava Customer Loyalty Agent",  # Name of the agent
-        instructions=CL_PROMPT,  # Instructions for the agent
-        toolset=toolset,
-        )
-        print(f"Created agent, ID: {agent.id}")
+            )
+            print(f"Created {env_var_name} agent, ID: {agent.id}")
 
 
