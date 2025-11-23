dc555a018f8332e8cd6df76ca48c8ed9603b4c8d
nmhtet <36500446+nmhtet@users.noreply.github.com>
2025-11-23 18:24:03 +0800

    Refactor agent initialization and streamline code

--- src/app/agents/customerLoyaltyAgent_initializer.py
+++ src/app/agents/customerLoyaltyAgent_initializer.py
@@ -12,9 +12,11 @@ load_dotenv()
 
 
 
+
 CL_PROMPT_TARGET = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), 'prompts', 'CustomerLoyaltyAgentPrompt.txt')
 with open(CL_PROMPT_TARGET, 'r', encoding='utf-8') as file:
     CL_PROMPT = file.read()
+
 
 
 
@@ -27,6 +29,7 @@ project_client = AIProjectClient(
 
 
 
+
 # Define the set of user-defined callable functions to use as tools (from MCP client)
 functions = create_function_tool_for_agent("customer_loyalty")
 toolset = ToolSet()
@@ -36,31 +39,11 @@ project_client.agents.enable_auto_function_calls(tools=functions)
 
 
 
-def initialize_agent(project_client : AIProjectClient, model : str, env_var_name : str, name : str, instructions : str, toolset : ToolSet):
-    agent_id = os.environ[env_var_name]
-    with project_client:
-        agent_exists = False
-        if agent_id:
-            # Check if agent exists.
-            agent = project_client.agents.get_agent(agent_id)
-            print(f"Retrieved existing agent, ID: {agent.id}")
-            agent_exists = True
-        
-        if agent_exists:
-            agent = project_client.agents.update_agent(
-                agent_id=agent.id,
-                model=model,
-                name=name,
-                instructions=instructions,
-                toolset=toolset
-            )
-            print(f"Updated {env_var_name} agent, ID: {agent.id}")
-        else:
-            agent = project_client.agents.create_agent(
-            model=model,
-            name=name,
-            instructions=instructions,
-            toolset=toolset
-            )
-            print(f"Created {env_var_name} agent, ID: {agent.id}")
-
+initialize_agent(
+    project_client=project_client,
+    model=os.environ["AZURE_AI_AGENT_MODEL_DEPLOYMENT_NAME"],
+    env_var_name="customer_loyalty",
+    name="Zava Customer Loyalty Agent",
+    instructions=CL_PROMPT,
+    toolset=toolset
+)
